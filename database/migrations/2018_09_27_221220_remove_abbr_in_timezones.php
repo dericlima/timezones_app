86575d971a7ea1544b1c164a0a78ad92a9aa1344
Deric Bacelar <dericlima@gmail.com>
2018-09-28 00:16:29 +0200

    Add migration to remove unused column in timezones

---
+++ database/migrations/2018_09_27_221220_remove_abbr_in_timezones.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveAbbrInTimezones extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('timezones', function (Blueprint $table) {
+           $table->dropColumn('abbr');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('timezones', function (Blueprint $table) {
+            $table->string('abbr');
+        });
+    }
+}
