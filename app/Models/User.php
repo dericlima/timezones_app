674fdd4f1143210db2356f9a75e08d3db554a737
Deric Bacelar <dericlima@gmail.com>
2018-10-03 19:08:44 +0200

    Improvements in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token'
     ];
 
     /**
@@ -68,8 +68,7 @@ class User extends Authenticatable implements JWTSubject
      */
     public function cities()
     {
-        return $this->belongsToMany(City::class, 'cities_users')
-            ->whereNull('deleted_at');
+        return $this->belongsToMany(City::class, 'cities_users');
     }
 
     /**
